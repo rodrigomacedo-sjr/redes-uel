c4a7d34ba2e77c5bf59d50f6f1087aa5c8a9ca18
JCSvg <jcscuni5570@gmail.com>
2025-04-01 11:35:34 -0300

    feat(menu): criar arquivo para gerenciar o menu do usuário (Cliente)

---
+++ src/menu.py
@@ -0,0 +1,9 @@
+"""
+Arquivo responsável por gerir o menu do usuário (Cliente).
+Este módulo contém funções para exibir opções e interagir com o usuário.
+
+Funções:
+    init() -- Função de inicialização do menu (ainda não implementada).
+"""
+def init():
+    pass
