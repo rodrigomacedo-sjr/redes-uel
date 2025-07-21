4e02ee3428b983dd1193626cf2101d203018e126
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-07-21 07:50:47 -0300

    fix: erro de conexao encerra apropriadamente

--- teste_de_rede/src/main.py
+++ teste_de_rede/src/main.py
@@ -17,11 +17,15 @@ def main():
         if tipo_transf == 1: # Upload
             destinatario = (IP_CONEXAO, PORT_CONEXAO)
             resultado = transferencia_tcp.enviar_pacotes(destinatario)
+            if not resultado:
+                resultado = {"quantidade_enviados": 0, "perdidos": 0, "tempo": 20}
             pc_enviados = resultado["quantidade_enviados"]
             perdidos = resultado["perdidos"]
         else: # Download
             servidor = (IP_LOCAL, PORT_LOCAL)
             resultado = transferencia_tcp.receber_pacotes(servidor)
+            if not resultado:
+                resultado = {"quantidade_enviados": 0, "perdidos": 0, "tempo": 20}
             pc_enviados = resultado["quantidade_enviados"]
             perdidos = resultado["perdidos"]
     
@@ -29,12 +33,16 @@ def main():
         if tipo_transf == 1: # Upload
             destinatario = (IP_CONEXAO, PORT_CONEXAO)
             resultado = transferencia_udp.enviar_pacotes(destinatario)
+            if not resultado:
+                resultado = {"quantidade_enviados": 0, "perdidos": 0, "tempo": 20}
             pc_enviados = resultado["quantidade_enviados"]
             perdidos = resultado["perdidos"]
         else: # Download
             servidor = (IP_LOCAL, PORT_LOCAL)
             resultado = transferencia_udp.receber_pacotes(servidor)
-            pc_enviados = resultado["quantidade_enviados"]
+            if not resultado:
+                resultado = {"quantidade_enviados": 0, "perdidos": 0, "tempo": 20}
+            pc_enviados = resultado.get("quantidade_enviados", 0)
             perdidos = resultado.get("perdidos_calculado", resultado.get("perdidos", 0))
 
     menu.output_estatisticas(pc_enviados, perdidos, resultado.get("tempo", 20))
